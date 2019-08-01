5fb58218b274363f9d6b7c428e6d8d1581387a2c
Mohammed Noori <kmanoorib@gmail.com>
2019-08-01 02:31:57 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,13 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     render json: PostSerializer.serialize(post)
   end
+  
+  def post_data
+    post = Post.find(params[:id])
+    #render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id],
+                              include: [ author: { only: [:name]}])
+  end
 
   private
 
