0e12830e967f75f936dbd3817141650365a17ce3
Mohammed Noori <kmanoorib@gmail.com>
2019-08-01 02:32:33 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,11 +24,11 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def post_data
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
-  end
-  
+  #def post_data
+  #  post = Post.find(params[:id])
+  #  render json: PostSerializer.serialize(post)
+  #end
+  # from lecture
   def post_data
     post = Post.find(params[:id])
     #render json: PostSerializer.serialize(post)
